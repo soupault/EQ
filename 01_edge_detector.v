6de77c34783c81c1ea7a0bd3f89a0f20cf599a39
Panfilov Egor <multicolor.mood@gmail.com>
2013-01-11 15:56:35 +0400

    good interface modules checked with poor tbs

--- 01_edge_detector.v
+++ 01_edge_detector.v
@@ -1,42 +1,59 @@
-module edge_detector (i_spdif, i_rst_n, i_clk, i_ena, o_zero, o_one, o_head, o_shift_ena);
-
-input wire i_spdif;
-input wire i_rst_n;
-input wire i_clk;
-input wire i_ena;
-
-output reg o_zero;
-output reg o_one;
-output reg o_head;
-output reg o_shift_ena;
+module edge_detector (
+	input wire i_spdif,
+	input wire i_rst_n,
+	input wire i_clk,
+//	input wire i_ena,
+	
+	output reg o_zero,
+	output reg o_one,
+	output reg o_head,
+	output reg o_shift_ena
+);
 
 wire is_edge;
-reg ff_bfr;
+reg [2:0] ff_bfr;
 reg [4:0] counter;
+reg state;
 
-assign is_edge = ff_bfr ^ i_spdif; // xor
+assign is_edge = ff_bfr[2] ^ ff_bfr[1]; // xor
 
 always @(posedge i_clk or negedge i_rst_n)
 begin
 	if (~i_rst_n) begin
+		state <= 1'b0;
 		counter <= 5'd0;
+		ff_bfr <= 3'd0;
 		o_zero <= 1'b0;
 		o_one <= 1'b0;
 		o_head <= 1'b0;
 		o_shift_ena <= 1'b0;
 	end else begin
-		if (i_ena) begin
-			ff_bfr <= i_spdif;
+	//	if (i_ena) begin
+		/* Metastability */
+		ff_bfr[2] <= ff_bfr[1];
+		ff_bfr[1] <= ff_bfr[0];
+		ff_bfr[0] <= i_spdif;
+		
+		case(state)
+		0: begin
+			if (is_edge)
+				state <= 1'b1;
+		end
+		
+		1: begin
 			if (is_edge) begin
 				counter <= 0;
-				o_shift_ena <= 1'b1;
-				if ((counter >= 5'd3) | (counter <= 5'd7))
+					
+				if ((counter >= 5'd3) & (counter <= 5'd7)) begin
+					o_shift_ena <= 1'b1;
 					o_zero <= 1'b1;
-				else if ((counter >= 5'd9) | (counter <= 5'd13))
+				end else if ((counter >= 5'd9) & (counter <= 5'd13)) begin
+					o_shift_ena <= 1'b1;
 					o_one <= 1'b1;
-				else if ((counter >= 5'd15) | (counter <= 5'd19))
+				end else if ((counter >= 5'd15) & (counter <= 5'd19)) begin
+					o_shift_ena <= 1'b1;
 					o_head <= 1'b1;
-				// else
+				end
 			end else begin
 				counter <= counter + 1'b1;
 				o_zero <= 1'b0;
@@ -45,6 +62,9 @@ begin
 				o_shift_ena <= 1'b0;
 			end
 		end
+		endcase
+		
+	//	end
 	end
 end
 
--- 02_assembly.v
+++ 02_assembly.v
@@ -1,54 +1,91 @@
-module assembly (i_zero, i_one, i_head, i_rst_n, i_clk, i_ena, o_package, o_ready);
+module assembly (
+	input wire i_zero,
+	input wire i_one,
+	input wire i_head,
+	input wire i_rst_n,
+	input wire i_clk,
+	input wire i_ena,
 
-input wire i_zero;
-input wire i_one;
-input wire i_head;
-input wire i_rst_n;
-input wire i_clk;
-input wire i_ena;
+	output reg [27:0] o_package,
+	output reg o_ready
+);
 
-output reg [// : ] o_package;
-output reg o_ready;
+reg [7:0] shift_reg;
+reg [55:0] package_reg;
+reg [5:0] counter;
+reg [6:0] i;
 
-reg [7:0] preamble; // 2 bits per char
-reg [??] data;
+reg det_X;
+reg det_Y;
+reg det_Z;
+
+// TODO: add 2 bits of preamble code in output reg
+// TODO: filling output reg with fixed frequency
+// !!! TODO: check if pkg[i] and pkg[i+1] are equal for even i
+
+localparam pre_X = 8'b11100010;
+localparam pre_Y = 8'b11100100;
+localparam pre_Z = 8'b11101000;
+
+// localparam pattern_0 = 4'b0110;
+// localparam pattern_1 = 3'b010;
+localparam pattern_0 = 2'b11;
+localparam pattern_1 = 2'b10;
 
 always @(posedge i_clk or negedge i_rst_n)
 begin
 	if (~i_rst_n) begin
-// 
+		o_package = 28'd0;
+		o_ready = 1'b0;
+		shift_reg = 8'd0;
+		package_reg = 28'd0;
+		counter = 6'd0;
+		det_X = 1'b0;
+		det_Y = 1'b0;
+		det_Z = 1'b0;
 	end else begin
 		if (i_ena) begin
-			if (i_head) begin
-			// reset data_reg
 			
+			/* Create data-flow */
 			case ({i_head, i_one, i_zero})
-				3'b001: begin
-					preamble <= {preamble[5:0], 2'b00}
-				end
-				
-				3'b010: begin
-					preamble <= {preamble[5:0], 2'b11}
-				end
-				
-				3'b100: begin
-					preamble <= {preamble[5:0], 2'b01}
-				end
+				3'b001: shift_reg <= {shift_reg[5:0], {2{~shift_reg[0]}}};
+				3'b010: shift_reg <= {shift_reg[6:0], ~shift_reg[0]};
+				3'b100: shift_reg <= {shift_reg[4:0], {3{~shift_reg[0]}}};
+				default: ;
 			endcase
 			
-			case (preamble)
-				8'b01011111: begin  // X
-					
+			/* Classify preamble */
+			if ((shift_reg == pre_X) || (shift_reg == ~pre_X)) det_X <= 1'b1;
+				else det_X <= 1'b0;
+			if ((shift_reg == pre_Y) || (shift_reg == ~pre_Y)) det_Y <= 1'b1;
+				else det_Y <= 1'b0;
+			if ((shift_reg == pre_Z) || (shift_reg == ~pre_Z)) det_Z <= 1'b1;
+				else det_Z <= 1'b0;
+			
+			//if ((det_X || det_Y) || det_Z) 
+			//	counter <= 6'd0;
+			
+			/* Try to latch correct package on next preamble */
+			if (i_head & (counter == 6'd54)) begin
+				for (i = 0; i < 28; i = i+1) begin
+					o_package[i] <= package_reg[i<<1];
 				end
-				
-				8'b01001100: begin  // Y
-					
-				end
-				
-				8'b01111101: begin  // Z
-					
-				end
-			endcase
+				o_ready <= 1'b1;
+			end else
+				o_ready <= 1'b0;
+			
+			/* Decode and create audio-block */
+			if ((shift_reg[1:0] == pattern_0) || (shift_reg[1:0] == ~pattern_0)) begin
+				package_reg <= {package_reg[54:0], 1'b0};
+			end
+			if ((shift_reg[1:0] == pattern_1) || (shift_reg[1:0] == ~pattern_1)) begin
+				package_reg <= {package_reg[54:0], 1'b1};
+			end
+			
+			/* Restart packager module */
+			// i_HEAD IS A WIRE !
+			if (((det_X || det_Y) || det_Z) || (i_head & (counter == 6'd54))) counter <= 6'd0;	
+			else if (((shift_reg[1:0] == pattern_0) || (shift_reg[1:0] == ~pattern_0)) || ((shift_reg[1:0] == pattern_1) || (shift_reg[1:0] == ~pattern_1))) counter <= counter + 1'b1;
 			
 		end
 	end
